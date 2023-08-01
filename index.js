8e76c26600cbe00c44fffc21ff11e3ca9aff8e94
Bdkoder <bdkoder360@gmail.com>
2023-08-02 02:37:46 +0600

    implementing chat api with socket

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-// index.js
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./app/routes/userRoutes");
 const profileRoutes = require("./app/routes/profileRoutes");
 const workExperienceRoutes = require("./app/routes/workExperienceRoutes");
 const categoryRoutes = require("./app/routes/categoryRoutes");
-const httpServer = require('http').createServer(app);
-const io = require('socket.io')(httpServer);
+const chatRoutes = require('./app/routes/chatRoutes');
+
 
 
 const app = express();
-
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 // Middleware
 app.use(bodyParser.json());
 
@@ -20,9 +19,7 @@ app.use('/users', userRoutes); // Mount userRoutes at '/users' endpoint
 app.use('/profile', profileRoutes); // Mount profileRoutes at '/profile' endpoint
 app.use('/profile', workExperienceRoutes); // Mount workExperienceRoutes at '/profile' endpoint
 app.use('/categories', categoryRoutes); // Mount categoryRoutes at '/categories' endpoint
-
-
-// ...
+app.use('/chat', chatRoutes); // Mount chatRoutes at '/chat' endpoint
 
 // Add socket.io middleware to access user data in socket events
 io.use((socket, next) => {
@@ -34,15 +31,20 @@ io.use((socket, next) => {
 
 // Socket.io event handling
 io.on('connection', (socket) => {
+  
   console.log(`Socket connected: ${socket.id}`);
 
   // Handle incoming messages
   socket.on('chatMessage', (data) => {
     // Broadcast the message to all connected clients
-    io.emit('newMessage', { user: socket.user, message: data.message });
+    io.emit('newMessage', { user: data.user, message: data.message });
   });
 
-  // Handle other socket events here...
+  // Handle typing event
+  socket.on('typing', (data) => {
+    console.log('User typing:', data);
+    // Handle typing status here (e.g., update typing status in the database)
+  });
 
   // Handle disconnection
   socket.on('disconnect', () => {
@@ -50,18 +52,7 @@ io.on('connection', (socket) => {
   });
 });
 
-
-
-
-
-
-
-
-
-
-
-
 const PORT = 3000;
-app.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
